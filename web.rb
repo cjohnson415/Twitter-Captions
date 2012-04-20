cdacd6640863c37c10aad97e0a9d874b1bd70dfc
Charles Johnson <cjohnson415@gmail.com>
2012-04-20 02:44:05 -0700

    haml

--- web.rb
+++ web.rb
@@ -1,5 +1,23 @@
+require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 get '/' do
-  "Hello, world"
+  haml :index
 end
+
+__END__
+@@ layout
+%html
+  %head
+    %title Network Tools
+  %body
+    #header
+      %h1 Network Tools
+    #content
+      =yield
+
+@@ index
+%p
+  Welcome to Network Tools. Below is a list
+  of the tools available.
