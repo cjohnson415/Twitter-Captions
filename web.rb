510c68a262df2ee689318bc5963e1a1f4d64dd29
Charles Johnson <cjohnson415@gmail.com>
2012-04-22 01:15:59 -0700

    Produces images for tweets with minor modification of search string to remove @handle's

--- web.rb
+++ web.rb
@@ -5,30 +5,46 @@ require 'twitter'
 require 'googleajax'
 require 'json'
 
+DEFAULT_URL = "http://dessertdarling.com/wp-content/uploads/2012/02/sadface3.jpg"
+
 enable :sessions
 GoogleAjax.referrer = "localhost:5000"
 
 get '/' do
-	session["twitHandle"] ||= nil
-  	haml :ask
+	session['twitHandle'] ||= nil
+	haml :ask
 end
 
+#Stores the twitter handle as a session variable, and displays the ten most recent tweets
 post '/handleReceived' do
-	@tHandle = params["twitterHandle"]
-	@userTweets = Twitter.user_timeline(@tHandle)
+	tHandle = params["twitterHandle"]
+	session['twitHandle'] = tHandle
+	@tweets = Twitter.user_timeline(tHandle)
 	haml :displayTweets
 end
 
+#Searches and displays images with tweets as captions
 post '/imagefun' do
-	@imageHash = Hash.new
-	@userTweets.each do |tweet|
-		firstResult = GoogleAjax::Search.images("Hello World")[:results][0]
-		url = firstResult[:url]
-		@imageHash[tweet] = url
+	TweetImage = Struct.new(:tweet, :imageUrl)
+	@images = Array.new
+	tweets = Twitter.user_timeline(session['twitHandle'])
+	tweets.each do |tweet|
+		firstResult = GoogleAjax::Search.images(tweet.searchText)[:results][0]
+		if (firstResult == nil)
+			pair = TweetImage.new(tweet.text + "\n No pictures found for this tweet!", DEFAULT_URL ) 
+		else
+			pair = TweetImage.new(tweet.text, firstResult[:unescaped_url]) 
+		end
+		@images << pair
 	end
-	@caption = @userTweets[0].text
-	@image = @imageHash[tweet]
-	haml :displayImage
+	haml :tweetImages
+end
+
+#return a string suitable for a search (currently just removes @'s followed by characters)
+class Twitter::Status
+	def searchText
+		return self.text.gsub(/@\w*\s?/, '')
+	end
 end
 
 __END__
@@ -49,16 +65,26 @@ __END__
     %input{:type => "submit", :value => "Submit!"}
 
 @@displayTweets
-%p== So, your twitter handle is really #{@tHandle}? 
+%p== So, your twitter handle is really #{session['twitHandle']}? 
 %p== Well here are your most recent tweets: 
 %table 
-  - @userTweets.each do |tweet|
+  - @tweets.each do |tweet|
     %tr
       %td=tweet.text 
 %form{:action => "imagefun", :method => "post"}
   %p
     %input{:type => "submit", :value => "Have some fun!"}
 
-@@displayImage
-%p== Here's the tweet #{@caption}.
-%img{:src => @image, :alt => "Smiley face"} 
+@@tweetImages
+%p They say you can know a person by their tweets...
+%p
+%table{:class => "image"}
+	- @images.each do |image|
+		%tr
+			%td
+				%img{:src => image.imageUrl, :height => 200}
+		%tr
+			%td=image.tweet 
+		%tr
+			%td
+				%br
